7bc6fc54bc0936e3bb38dd661ac230da2ba3728b
JomWest <145591543+JomWest@users.noreply.github.com>
2024-06-24 22:09:33 -0600

    Update views

--- Matagalpa/Casas/Casas_Detalle/Casa_Molino_Norte/js/mensaje.js
+++ Matagalpa/Casas/Casas_Detalle/Casa_Molino_Norte/js/mensaje.js
@@ -1,14 +1,19 @@
 var whatsappButtons = document.querySelectorAll(".Ws-button");
-var imagePath = "assets/Casas-Recursos/Casa_Molino_Norte/fachada.jpg"; 
-var localServerURL = "http://127.0.0.1:5501/Matagalpa/Casas/Casas_Detalle/Casa_Molino_Norte/Casa_molino_norte.html";
+var imagePath = "/ruta/a/tu/imagen/fachada.jpg"; // Ruta de la imagen en tu sitio Netlify
+var netlifyURL = "https://raicesnicas.netlify.app"; // URL base de tu sitio Netlify
 
 whatsappButtons.forEach(function(button) {
     button.addEventListener("click", function() {
         var NOmbreCAsa = document.querySelector(".NombreCasa").textContent;
         var phoneNumber = "50587326292";
         var message = "Hola, estoy interesado en la " + NOmbreCAsa + ". ";
-        var pageURL = localServerURL + "?image=" + encodeURIComponent(imagePath);
-        message += pageURL;
+        message += "Aquí tienes más detalles: ";
+
+        // Crear el enlace directo a la imagen en tu sitio Netlify
+        var imageURL = netlifyURL + imagePath;
+
+        // Agregar el enlace a la imagen al mensaje
+        message += imageURL;
 
         var whatsappLink = "https://api.whatsapp.com/send?phone=" + phoneNumber + "&text=" + encodeURIComponent(message);
 
