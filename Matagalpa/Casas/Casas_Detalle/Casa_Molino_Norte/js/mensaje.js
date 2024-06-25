ea3881760943a964a7a7c3ddca6c9afe31c615ba
JomWest <145591543+JomWest@users.noreply.github.com>
2024-06-24 22:11:03 -0600

    Update views

--- Matagalpa/Casas/Casas_Detalle/Casa_Molino_Norte/js/mensaje.js
+++ Matagalpa/Casas/Casas_Detalle/Casa_Molino_Norte/js/mensaje.js
@@ -1,19 +1,11 @@
 var whatsappButtons = document.querySelectorAll(".Ws-button");
-var imagePath = "/ruta/a/tu/imagen/fachada.jpg"; // Ruta de la imagen en tu sitio Netlify
-var netlifyURL = "https://raicesnicas.netlify.app"; // URL base de tu sitio Netlify
 
 whatsappButtons.forEach(function(button) {
     button.addEventListener("click", function() {
         var NOmbreCAsa = document.querySelector(".NombreCasa").textContent;
         var phoneNumber = "50587326292";
         var message = "Hola, estoy interesado en la " + NOmbreCAsa + ". ";
-        message += "Aquí tienes más detalles: ";
-
-        // Crear el enlace directo a la imagen en tu sitio Netlify
-        var imageURL = netlifyURL + imagePath;
-
-        // Agregar el enlace a la imagen al mensaje
-        message += imageURL;
+        message += "¿Podrías proporcionarme más información la proiedad?\n";
 
         var whatsappLink = "https://api.whatsapp.com/send?phone=" + phoneNumber + "&text=" + encodeURIComponent(message);
 
