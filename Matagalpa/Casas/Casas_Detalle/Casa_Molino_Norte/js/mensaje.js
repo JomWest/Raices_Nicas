53c8b49314306f0249c8c5579a31c265223ce167
JomWest <145591543+JomWest@users.noreply.github.com>
2024-07-09 16:55:49 -0600

    Actualizacion y arrelos de errores en el formulario

--- Matagalpa/Casas/Casas_Detalle/Casa_Molino_Norte/js/mensaje.js
+++ Matagalpa/Casas/Casas_Detalle/Casa_Molino_Norte/js/mensaje.js
@@ -1,13 +1,14 @@
 var whatsappButtons = document.querySelectorAll(".Ws-button");
-
+var currentURL = window.location.href;
 whatsappButtons.forEach(function(button) {
-    button.addEventListener("click", function() {
+    button.addEventListener("click", function(event) {
+        event.preventDefault();
         var NOmbreCAsa = document.querySelector(".NombreCasa").textContent;
         var phoneNumber = "50587326292";
         var message = "Hola, estoy interesado en la " + NOmbreCAsa + ". ";
         message += "¿Podrías proporcionarme más información la proiedad?\n";
 
-        var whatsappLink = "https://api.whatsapp.com/send?phone=" + phoneNumber + "&text=" + encodeURIComponent(message);
+        var whatsappLink = "https://api.whatsapp.com/send?phone=" + phoneNumber + "&text=" + encodeURIComponent(message) + currentURL;
 
         window.open(whatsappLink, "_blank");
     });
